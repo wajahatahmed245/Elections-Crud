bee66532b2b97aba58c6c38e44b44de204b3e1b4
wajahat ahmed <wajahat.ahmed245@gmail.com>
2018-06-10 05:49:28 -0700

    welcome handled ui

--- welcome.php
+++ welcome.php
@@ -1,28 +1,42 @@
-<div align="center" class"welcome col-sm-9 display-3">
-
 <?php
-$modalBody=null;
-$headingOfIndex=null;
-if ($_SESSION["userPresent"] != null) {
-    
-    $headingOfIndex=strtoupper($_SESSION["userPresent"]);
-    
-    $bagde='<h1> <span class="badge badge-success">'.$headingOfIndex.' INTERFACE</span></h1>';
-}
+  $modalBody=null;
+  $headingOfIndex=null;
+  $button=null;
+  $buttonEnd=null;
+  $since=null;
+  if ($_SESSION["userPresent"] != null) {
+      
+      $headingOfIndex=strtoupper($_SESSION["userPresent"]);
+      
+      $bagde='<h1> <span class="badge badge-success">'.$headingOfIndex.' INTERFACE</span></h1>';
+  }
+  
+  else{
+      $modalBody='Please Sign In  ..Or for your Vote click on Fill Form ';
+      $bagde="Management System";
+  }
+  
+  ?>
 
-else{
-    $modalBody='Please Sign In  ..Or for your Vote click on Fill Form ';
-    $bagde="Management System";
-}
-
-?>
+  
 <?php
 if ($_SESSION["userPresent"] == null){
-echo '<button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">';
-echo   ' Since October 6, 1993';
- echo '</button>';
+    $button='<button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">';
+    $since= ' Since October 6, 1993';
+    $buttonEnd='</button>';
 }
 ?>
+
+
+<aside style="width:855px" align="center" class"welcome col-sm-9 display-3">
+  <?=  $button  ?>
+  <?=  $since  ?>
+  <?=  $buttonEnd  ?>
+  
+
+<h1 class="text-xs-center"  ><?=  $bagde  ?> </h1>
+
+</aside>
    <!-- The Modal -->
    <div class="modal fade" id="myModal">
     <div class="modal-dialog">
@@ -47,6 +61,3 @@ echo   ' Since October 6, 1993';
       </div>
     </div>
     </div>
-<h1 class="text-xs-center"  ><?=  $bagde  ?> </h1>
-
-</div>
